9c4f4556291b5c5702a1bc72a9cf73467cb6647a
Shreyansh Goyal <shreyanshg2010@gmail.com>
2018-09-28 04:05:29 +0530

    Create Add_one_to_the_array.py

---
+++ Array/Add_one_to_the_array.py
@@ -0,0 +1,19 @@
+def plusOne(A):
+    for i in range(len(A)-1, -1, -1):
+        if A[i] == 9:
+             A[i] = 0
+        else:
+            A[i] += 1
+            print(A[i])
+            while True:
+                if A[0] == 0:
+                    del A[0]
+                else:
+                    return A
+            return A
+    A = [1] + A
+    return A
+s = input()
+A = list(map(int, s.split()))
+print(plusOne(A))
+
