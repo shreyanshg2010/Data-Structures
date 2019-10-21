ae13dbbe2d74e87f7b04df27009a691082dbfb0c
Shreyansh Goyal <shreyanshg2010@gmail.com>
2019-10-21 15:40:37 +0530

    Create Longest semi alternating substring.java

---
+++ Codelity/American Express/Longest semi alternating substring.java
@@ -0,0 +1,27 @@
+You are given a string s of length n containing only characters a and b. A substring of s called a semi-alternating substring if it does not contain three identical consecutive characters. Return the length of the longest semi-alternating substring.
+
+Example 1:
+
+Input: "baaabbabbb"
+Output: 7
+Explanation: "aabbabb"
+Example 2:
+
+Input: "abaaaa"
+Output: 4
+Explanation: "abaa"
+
+
+
+
+  private int findLongestSemiAlteringSubstring(String str) {
+        int len = 0;
+        if(str == null) return 0;
+        if(str.length() < 3) return str.length();
+        for(int i=2,j=0;i<str.length(); ++i) {
+            if(str.charAt(i) == str.charAt(i-1) && str.charAt(i) == str.charAt(i-2))
+                j = i-1;
+            len = Math.max(len, i-j+1);
+        }
+        return len;
+    }
